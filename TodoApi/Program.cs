1b7aedb1d834f0db8b7887dcab68af3283bd0077
supaporn <supapornb2020@gmail.com>
2026-09-12 17:18:06 +0700

    [dev] mapget todo id

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,3 +1,5 @@
+
+using TodoApi.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,7 +16,27 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+var todos = new List<TodoGetDto>
+{
+    new(1, "Learn Minimal API", false),
+    new(2, "Learn Vue", false)
+};
+
 app.MapGet("/", () => "Hello Todo API");
+
+app.MapGet("/api/todos", () =>
+    Results.Ok(todos));
+
+app.MapGet("/api/todos/{id}", (int id) =>
+{
+    var todo = todos.FirstOrDefault(x => x.Id == id);
+
+    return todo is null
+        ? Results.NotFound()
+        : Results.Ok(todo);
+});
+
+
 
 app.Run();
 
